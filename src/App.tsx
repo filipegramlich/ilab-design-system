bb0f8b867906b0eec8a9d70168a979adc3885f9f
filipe <filipe.ogramlich@gmail.com>
2022-10-15 19:39:32 -0300

    storybook-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './styles/global.css';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1 className="font-bold text-5xl text-violet-500"> Hello World</h1>
+
+      <button className='bg-violet-600 font-medium px-5 py-2 rounded text-white hover:bg-violet-700'>
+        Enviar
+      </button>
+    </>
   )
 }
 
